3c5caab0a19cb09958ce6219ccbc6a2b38da9f1c
FrDacey <minesow8@gmail.com>
2023-10-25 12:34:19 +0200

    Super Enhanced Game

--- courses/main.py
+++ courses/main.py
@@ -1,15 +1,15 @@
 import random
     
-print("Bienvenue, Veuillez choisir votre niveau \nFacile : 0 à 10. \nMoyen : 0 à 100. \nDifficile : 0 à 1000.\n\n")
+print("Bienvenue, Veuillez choisir votre niveau \nFacile(1) : 0 à 10. \nMoyen(2) : 0 à 100. \nDifficile(3) : 0 à 1000.\n\n")
 nbressaie = 0
 difficulter = input("Veuillez choisir votre difficulté : ")
-while difficulter != "Facile" and difficulter != "Moyen" and difficulter != "Difficile":
+while difficulter != "Facile" and difficulter != "Moyen" and difficulter != "Difficile" and difficulter != "1" and difficulter !=  "2" and difficulter != "3":
     print("\nErreur la difficulter n'est pas trouver. \nVeuillez choisir une difficulter valide.\n\n")
     difficulter = input("Veuillez choisir votre difficulté : ")
 print("Bon jeu !\n")
 numeroutilisateur = 0
 
-if difficulter == "Facile" :
+if difficulter == "Facile" or difficulter == '1' :
     nombre_a_trouver = random.randint(1, 10)
     while nombre_a_trouver != numeroutilisateur :
         try: 
@@ -22,7 +22,7 @@ if difficulter == "Facile" :
         except ValueError:
             print("Erreur ! Vous devez entrer un nombre Valide.")
 
-elif difficulter == "Moyen" :
+elif difficulter == "Moyen" or difficulter == 2 :
     nombre_a_trouver = random.randint(1, 100)
     while nombre_a_trouver != numeroutilisateur :
         try: 
@@ -35,7 +35,7 @@ elif difficulter == "Moyen" :
         except ValueError:
             print("Erreur ! Vous devez entrer un nombre Valide.")
 
-elif difficulter == "Difficile" :
+elif difficulter == "Difficile" or difficulter == 3 :
     nombre_a_trouver = random.randint(1, 1000)
     while nombre_a_trouver != usernumber :
         try: 
