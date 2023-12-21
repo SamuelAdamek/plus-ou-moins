b844563af6696850dc84d3afada05bee13fc4287
Dacey <minesow8@gmail.com>
2023-12-21 14:01:25 +0100

    Create customers.py

---
+++ courses/customers.py
@@ -0,0 +1,24 @@
+clients = {
+
+    "repertoirenumb1": {
+        
+        "Nom": "Doe" ,
+        "Prenom": "John",
+        "Age": 21 ,
+        "Email": "John.Doe@xyz.com",
+        "Hobbies": "Karaté, Tennis"
+    },
+    "repertoirenumb2": {
+        "Nom": "Stewart" ,
+        "Prenom": "Jane",
+        "Age": 26 ,
+        "Email": "[Email non renseigné]",
+        "Hobbies": "Danse, Peinture, Chant"
+    },
+    "repertoirenumb3": {
+        "Nom": "Olivier" ,
+        "Prenom": "Tardieu",
+        "Age": 32 ,
+        "Email": "Olivier.Tardieu@xyz.com",
+        "Hobbies": "[Hobbies non renseignés]"
+    }}
